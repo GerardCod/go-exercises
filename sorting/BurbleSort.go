03bc9a94911730f0fac910f7c28799cec6f2cbf4
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-15 20:54:29 -0500

    modify: BurbleSort algorithm implemented

--- sorting/BurbleSort.go
+++ sorting/BurbleSort.go
@@ -1,1 +1,22 @@
 package sorting
+
+type BurbleSort struct{}
+
+func (bs *BurbleSort) Sort(data []int) []int {
+
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data)-1; j++ {
+			if data[j+1] < data[j] {
+				bs.swap(data, j, j+1)
+			}
+		}
+	}
+
+	return data
+}
+
+func (bs *BurbleSort) swap(data []int, left, right int) {
+	aux := data[right]
+	data[right] = data[left]
+	data[left] = aux
+}
