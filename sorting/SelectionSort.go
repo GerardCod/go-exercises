d8f57675ae088416f698d73b4512beab43bd538c
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-15 21:09:37 -0500

    modify: SelectionSort algorithm implemented

--- sorting/SelectionSort.go
+++ sorting/SelectionSort.go
@@ -1,1 +1,24 @@
 package sorting
+
+type SelectionSort struct{}
+
+// Sort
+func (ss *SelectionSort) Sort(data []int) []int {
+	dataCopy := data
+
+	for i := 0; i < len(dataCopy)-1; i++ {
+		minimumIdx := i
+
+		for j := i + 1; j < len(dataCopy)-1; j++ {
+			if dataCopy[j] < dataCopy[minimumIdx] {
+				minimumIdx = j
+			}
+		}
+
+		if minimumIdx != i {
+			swap(dataCopy, i, minimumIdx)
+		}
+	}
+
+	return dataCopy
+}
